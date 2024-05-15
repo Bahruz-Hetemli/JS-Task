aa703094986c092b2a33820c224f0e68bc466029
Bahruz-Hetemli <hetemli53@gmail.com>
2024-05-15 10:56:21 +0400

    JS/Task

---
+++ main.js
@@ -0,0 +1,28 @@
+
+function pResult() {
+    let input1 = document.getElementById("text1");
+    let input2 = document.getElementById("text2");
+    let output = document.getElementById("p-output");
+    
+    output.innerHTML = (Number(input1.value) + Number(input2.value))*2;
+    
+}
+
+function sResult() {
+    let input1 = document.getElementById("text1");
+    let input2 = document.getElementById("text2");
+    let output = document.getElementById("s-output");
+
+    output.innerHTML = input1.value * input2.value;
+
+}
+
+
+let isim=["behruz", "Fatime", "seymur", "Ayse"];
+
+
+function quvvet(eded, ustu){
+    let cavab=Math.pow(eded,ustu)
+    console.log(cavab);
+  }
+  quvvet()
